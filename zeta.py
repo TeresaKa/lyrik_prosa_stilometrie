d456e7f3391a6b01f40277673b4b4472bb523c5f
piahh <64070643+piahh@users.noreply.github.com>
2020-06-09 10:34:08 +0200

    Add files via upload
    Große txt Datei wird erstellt
    kleine Segmente (grad 100 Wörter) -> keine Ahnung warum es sagt, dass der Index out of range ist.. Es machts nämlich trotzdem :D 
    nimmt Sonderzeichen raus (,;.:?!"(){}[]/-) dann haben wir wirklich nur die Wörter
    
    Muss am Dateinamen der Segmente noch etwas basteln, damit die dann klar zugeordnet werden können
    Stopwords sind noch drin, passt das oder sollen die rausgenommen werden für zeta?

--- zeta.py
+++ zeta.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import os
 import glob
+import os
 
-#Pseudo-Code
 
-### fügt alle txt-Datein zu einer großen zusammen. Text nicht in einer Zeile
+
+### fügt alle txt-Datein zu einer Großen zusammen. Text nicht in einer Zeile
 
 read_files = glob.glob("C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/Arent_raw/*.txt")
 path = os.path.dirname('C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/Arent_raw/*.txt')
@@ -16,33 +17,6 @@ with open("C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/result.txt", "wb") a
 corpus_name = os.rename('result.txt', 'corpus_' + str(corpus) + '.txt')
 
 
-### nur der String, nicht die txt-Datei
-
-# def cutchunks(string, length):
-#     return (string[0+i:length+i] for i in range(0, len(string), length))
-#
-# text = ('C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/corpus_Arent_raw.txt')
-# lines = (i.strip() for i in text.splitlines())
-# for line in lines:
-#     for chunk in cutchunks(line, 5):
-#         print(chunk)
-
-
-### nach Zeile, nicht nach Wörtern
-
-def chunks(t, n):
-    for i in range(0, len(t), n):
-        yield t[i:i + n]
-
-tokens = []
-with open('C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/corpus_Arent_raw.txt') as main_file:
-    for token in main_file:
-        tokens.append(token)
-
-for i, group in enumerate(chunks(tokens, n=100), start=1):
-    with open('file%d.txt' % i, mode="w") as out_file:
-        for line in group:
-            out_file.write(token)
 
 
 ### zählt Wörter der txt-Datei
@@ -53,41 +27,37 @@ words = data.split()
 print('Number of words', len(words))
 
 
-### 3522 Datein mit der Wiederholung der ersten Zeile
-
-# def chunks(t, n):
-#     for i in range(0, len(t), n):
-#          yield t[i:i + n]
-#
-# with open('C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/corpus_Arent_raw.txt') as file:
-#      for token in file:
-#          data = file.read()
-#          words = data.split()
-#
-# for i, group in enumerate(chunks(data, n=100)):
-#     with open('file%d.txt' % i, mode="w") as out_file:
-#        for line in group:
-#            out_file.write(token)
+def replace(string, ffilter):
+    for char in ffilter:
+        string = string.replace(char, "")
+    return string
 
 
-### zählt alle Wörter auf (print in PyCharm, nicht gespeichert)
+def segment(wordList, n):
+    for i in range(0, len(wordList), n):
+        datei = []
+        for j in range(i, i+n):
+            datei.append(wordList[j])
 
-with open('C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/corpus_Arent_raw.txt', "rt") as f:
-    for line in f:
-        for word in line.split():
-            print(word)
+        if os.path.exists("Segment"+str(i)+".txt"):
+            txtFile = open("Segment"+str(i)+".txt", "w")
+            txtFile.write("")
+            txtFile.close()
 
-
-### erstellt eine Datei mit dem letzten Wort
-
-# with open('C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/corpus_Arent_raw.txt', "rt") as f:
-#     for line in f:
-#         for word in line.split():
-#             with open('file%d.txt', mode="w") as out_file:
-#                 out_file.write(word)
-
-
-
+        txtFile = open("Segment"+str(i)+".txt", "a")
+        for word in datei:
+            txtFile.write(word + " ")
+        txtFile.close()
+    return
 
 
+file = open("C:/Users/pinaj/Documents/Uni/DH/Stilometrie/Code/corpus_Arent_raw.txt", "r")
+data = file.read()
 
+charFilter = ",.!;:—?-_\"(){}[]/\\"
+
+
+data = replace(data, charFilter)
+
+
+segment(data.split(), 99)
